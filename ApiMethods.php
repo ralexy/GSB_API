ef1e331de1f99fc9a30200d20879a8b671eb339d
Alexy ROUSSEAU <contact@alexy-rousseau.com>
2019-03-26 20:26:17 +0100

    Ajout de la synchronisation de données à l'API

--- ApiMethods.php
+++ ApiMethods.php
@@ -14,14 +14,22 @@ class ApiMethods
      * Constantes contenant les messages API
      * Utile pour débogguer l'Api et comprendre précisément ce qui ne va pas en cas de problème
      */
-    const UNDEFINED_ERROR     = 'Erreur non définie';
+    const UNDEFINED_ERROR = 'Erreur non définie';
     const INVALID_CREDENTIALS = 'Identifiant ou mot de passe incorrect';
-    const VALID_CREDENTIALS   = 'Connexion réussie';
+    const VALID_CREDENTIALS = 'Connexion réussie';
+    const SYNCHRONIZE_OK = 'Synchronisation réussie';
 
+    /**
+     * Constantes permettant de déterminer si on créé ou non une FDF
+     * et des frais HF
+     */
+    private $createRecord;
+    private $createRecordHf;
     private $pdo;
     private $result = ['result' => false];
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->pdo = PdoGsb::getInstance();
     }
 
@@ -33,14 +41,15 @@ class ApiMethods
      * @param $password Son mot de passe
      * @return array result (la réponse API au format tableau, contient l'id du membre si ses ids sont exacts)
      */
-    public function login($username, $password) {
+    public function login($username, $password)
+    {
         $query = $this->pdo->prepare('SELECT id, mdp FROM membre WHERE login = :login');
 
         $query->bindParam(':login', $username, PDO::PARAM_STR);
         $query->execute();
         $data = $query->fetch();
 
-        if(isset($data['mdp']) && password_verify($password, $data['mdp'])) {
+        if (isset($data['mdp']) && password_verify($password, $data['mdp'])) {
             $result['result'] = true;
             $result['message'] = self::VALID_CREDENTIALS;
             $result['idmembre'] = $data['id'];
@@ -51,102 +60,184 @@ class ApiMethods
         return $result;
     }
 
-
-    public function synchronize($memberId, $expenses) {
-        $expenses = json_decode($expenses);
+    /**
+     * Méthode appellée par l'API qui synchronise les données transmises par l'APP à la base de données
+     *
+     * @param $memberId
+     * @param $expenses
+     */
+    public function synchronize($memberId, $expenses)
+    {
+        $expenses = json_decode($expenses, true);
 
         // {"201904":{"annee":2019,"etape":0,"km":50,"lesFraisHf":[],"mois":4,"nuitee":0,"repas":0},"201903":{"annee":2019,"etape":0,"km":20,"lesFraisHf":[{"jour":21,"montant":120.0,"motif":""}],"mois":3,"nuitee":30,"repas":0}}
 
-        foreach($expenses as $key => $value) {
+        foreach ($expenses as $expenseLine) {
             /*
-             * Vérification si la fiche de frais existe déjà ou non
-             * Si elle n'existe pas on la créé
-             * Si elle existe on la met à jour
+             * Création du mois sous la forme YYYYMM
+             * Ajout au tableau $expenseLine pour être utilisé aisément dans les fonctions appellées
              */
-           $query1 = $this->pdo->prepare('SELECT COUNT(id) FROM fichefrais WHERE idmembre = :memberId');
-           $query1->bindValue(':memberId', $memberId);
-           $expenseReportExists = $query1->fetchColumn();
+            $expenseLine['moisAnnee'] = $expenseLine['annee'] . sprintf("%02d", $expenseLine['mois']);
 
-           if(!$expenseReportExists) {
-               $query2 = $this->pdo->prepare(
-                   'INSERT INTO fichefrais
-                              SET idmembre = :memberId, 
-                              mois = :mois,
-                              nbjustificatifs = :nbjustificatifs,
-                              montantvalide = 0,
-                              datemodif = NOW(),
-                              idEtat = `CR`,
-                              idVehicule = 1
-               ');
+            // Création de la FDF si elle n'existe pas
+            $this->createNewFicheFrais($memberId, $expenseLine);
 
-               $query2->bindParam(":memberId", $memberId, PDO::PARAM_STR);
-               $query2->bindParam(":mois", $key['mois'], PDO::PARAM_INT);
-               $query2->bindParam(":nbJustificatifs", $key['nbJustificatifs'], PDO::PARAM_INT);
+            // Insertion des frais forfait
+            $this->createOrUpdateFraisForfait($memberId, $expenseLine);
 
-               $query2->execute();
-               $query2->closeCursor();
+            // Insertion des frais HF
+            $this->createOrUpdateFraisHf($memberId, $expenseLine);
+        }
 
-               // Insertion des frais forfait
-               $fraisForfait = array(
-                   array('ETP' => $key['etape']),
-                   array('KM'  => $key['KM']),
-                   array('NUI' => $key['nuitee']),
-                   array('REP' => $key['repas'])
-               );
+        $result['message'] = self::SYNCHRONIZE_OK;
 
-               for($i = 0, $iMax = count($fraisForfait); $i < $iMax; $i++) {
+        return $result;
+    }
 
-                   $query3 = this->$this->pdo->prepare('INSERT INTO lignefraisforfait
-                                                                  SET idmembre = :memberId,
-                                                                  mois = :mois,
-                                                                  idfraisforfait = :idFraisForfait,
-                                                                  quantite = :quantite
+
+    private function createNewFicheFrais($memberId, $expenseLine)
+    {
+        /**
+         * Vérification si la fiche de frais existe déjà ou non
+         */
+        $query = $this->pdo->prepare('SELECT COUNT(idmembre) FROM fichefrais 
+                                                WHERE idmembre = :memberId
+                                                AND mois = :mois
+        ');
+        $query->bindValue(':memberId', $memberId);
+        $query->bindParam(":mois", $expenseLine['moisAnnee'], PDO::PARAM_INT);
+        $query->execute();
+
+        $this->createRecord = $query->fetchColumn();
+
+        /**
+         * Si la fiche de frais n'existe pas on la créé dans la table fichefrais
+         */
+        if (!$this->createRecord) {
+            $query2 = $this->pdo->prepare('INSERT INTO fichefrais
+                                                        SET idmembre = :memberId, 
+                                                        mois = :mois,
+                                                        nbjustificatifs = 0,
+                                                        montantvalide = 0,
+                                                        datemodif = NOW(),
+                                                        idEtat = \'CR\',
+                                                        idVehicule = 1
+            ');
+
+            $query2->bindParam(":memberId", $memberId, PDO::PARAM_STR);
+            $query2->bindParam(":mois", $expenseLine['moisAnnee'], PDO::PARAM_INT);
+
+            $query2->execute();
+            $query2->closeCursor();
+        }
+
+        $query->closeCursor();
+    }
+
+
+    private function createOrUpdateFraisForfait($memberId, $expenseLine)
+    {
+        $fraisForfait = array(
+            'ETP' => $expenseLine['etape'],
+            'KM' => $expenseLine['km'],
+            'NUI' => $expenseLine['nuitee'],
+            'REP' => $expenseLine['repas']
+        );
+
+        foreach ($fraisForfait as $key => $value) {
+            $query3 = $this->pdo->prepare('INSERT INTO lignefraisforfait
+                                                                 SET idmembre = :memberId,
+                                                                 mois = :mois,
+                                                                 idfraisforfait = :idFraisForfait,
+                                                                 quantite = :quantite
                    ');
+            $query3->bindValue(':memberId', $memberId, PDO::PARAM_STR);
+            $query3->bindValue(':mois', $expenseLine['moisAnnee']);
+            $query3->bindValue(':idFraisForfait', $key);
+            $query3->bindValue(':quantite', $value);
 
-                   $query3->bindValue(':memberId', $memberId, PDO::PARAM_STR);
-                   $query3->bindValue(':mois', $key['mois']),
-                   $query3->bindValue(':idFraisForfait', $fraisForfait[$i][0]);
-                   $query3->bindValue(':quantite', $fraisForfait[$i][1]);
+            $query3->execute();
+            $query3->closeCursor();
+        }
+    }
 
-                   $query3->execute();
-                   $query3->closeCursor();
-               }
+    /**
+     * Méthode permettant de créer un frais HF ou de le mettre à jour si il existe déjà
+     *
+     * @param $memberId
+     * @param $expenseLine
+     *
+     * @return array
+     */
+    private function createOrUpdateFraisHf($memberId, $expenseLine)
+    {
+        foreach ($expenseLine['lesFraisHf'] as $leFraisHf) {
+            /*
+             * Ajout de la date au tableau sous le format AAAA-MM-JJ
+             */
+            if(empty($leFraisHf['date'])) {
+                $leFraisHf['date'] = $expenseLine['annee'] . '-' . sprintf("%02d", $expenseLine['mois']) . '-' . $leFraisHf['jour'];
+            }
 
+            $query = $this->pdo->prepare('SELECT COUNT(id) FROM lignefraishorsforfait 
+                                                        WHERE idmembre = :memberId
+                                                        AND date = :date
+                                                        AND libelle = :libelle
+            ');
 
+            $query->bindValue(':memberId', $memberId, PDO::PARAM_INT);
+            $query->bindValue(':date', $leFraisHf['date'], PDO::PARAM_STR);
+            $query->bindValue(':libelle', $leFraisHf['motif'], PDO::PARAM_STR);
+            $query->execute();
 
-               // Insertion des frais HF
-               foreach($key['lesFraisHf'] as $leFraisHf) {
-                   $query3 = $this->pdo->prepare('INSERT INTO lignefraisHf 
-                                                            SET idmembre = :memberId,
-                                                            mois :mois,
-                                                            libelle = :libelle,
-                                                            date = :date,
-                                                            montant = :montant
-                   ');
+            $this->createRecordHf = $query->fetchColumn();
 
-                   $date = $key['annee']. '-'. $key['mois']. '-'. $leFraisHf['jour'];
+            if (!$this->createRecordHf) {
+                $query2 = $this->pdo->prepare('INSERT INTO lignefraishorsforfait 
+                                                         SET idmembre = :memberId,
+                                                         mois = :mois,
+                                                         libelle = :libelle,
+                                                         date = :date,
+                                                         montant = :montant
+                       ');
 
-                   $query3->bindValue(':memberId', $memberId, PDO::PARAM_STR);
-                   $query3->bindValue(':mois', $leFraisHf['mois'], PDO::PARAM_INT);
-                   $query3->bindValue(':libelle', $leFraisHf['motif'], PDO::PARAM_INT);
-                   $query3->bindValue(':date', $date, PDO::PARAM_STR);
-                   $query3->bindValue(':montant', $leFraisHf['montant'], PDO::PARAM_INT);
+                $query2->bindValue(':memberId', $memberId, PDO::PARAM_STR);
+                $query2->bindValue(':mois', $expenseLine['moisAnnee'], PDO::PARAM_INT);
+                $query2->bindValue(':libelle', $leFraisHf['motif'], PDO::PARAM_STR);
+                $query2->bindValue(':date', $leFraisHf['date'], PDO::PARAM_STR);
+                $query2->bindValue(':montant', $leFraisHf['montant'], PDO::PARAM_INT);
 
-                   $query3->execute();
-               }
-           }
+                $query2->execute();
+            } else {
+                $query2 = $this->pdo->prepare('UPDATE lignefraishorsforfait 
+                                                         SET idmembre = :memberId,
+                                                         mois = :mois,
+                                                         libelle = :libelle,
+                                                         date = :date,
+                                                         montant = :montant
+                ');
 
-           else {
+                $query2->bindValue(':libelle', $leFraisHf['motif'], PDO::PARAM_STR);
+                $query2->bindValue(':montant', $leFraisHf['montant'], PDO::PARAM_INT);
+                $query2->bindValue(':date', $leFraisHf['date'], PDO::PARAM_STR);
+                $query2->bindValue(':memberId', $memberId, PDO::PARAM_STR);
+                $query2->bindValue(':mois', $expenseLine['moisAnnee'], PDO::PARAM_INT);
 
-           }
+                $query2->execute();
+            }
+
+            $query->closeCursor();
+            $query2->closeCursor();
         }
     }
 
     /**
      * Méthode permettant de retourner une erreur générique
+     *
      * @return array
      */
-    public function getUndefinedError() {
+    public function getUndefinedError()
+    {
         $result['message'] = self::UNDEFINED_ERROR;
 
         return $result;
--- api.php
+++ api.php
@@ -17,6 +17,11 @@ if(isset($_GET['action'])) {
             if(isset($_GET['username']) && isset($_GET['password'])) {
                 $jsonResult = $apiMethods->login($_GET['username'], $_GET['password']);
             }
+
+        case 'synchronize':
+            if(isset($_GET['memberId']) && isset($_GET['expenses'])) {
+                $jsonResult = $apiMethods->synchronize($_GET['memberId'], $_GET['expenses']);
+            }
     }
 }
 
